548ab1e1909fbaa1537e3907e2fcdabc0997b98b
Ben Derham <1805685+benderham@users.noreply.github.com>
2023-01-13 16:32:35 +0800

    uncomment query code

--- src/components/UserList/UserListContainer.tsx
+++ src/components/UserList/UserListContainer.tsx
@@ -13,17 +13,17 @@ import { UserList } from './UserList';
 //   }
 // `;
 
-// const useGetUsers = () => {
-//   const { data, loading, error } = useQuery(GET_USERS);
-//   const users = data ? data.users : [];
-//   return { users, loading, error };
-// };
+const useGetUsers = () => {
+  const { data, loading, error } = useQuery(GET_USERS);
+  const users = data ? data.users : [];
+  return { users, loading, error };
+};
 
 export const UserListContainer = () => {
-  // const { users, loading, error } = useGetUsers();
+  const { users, loading, error } = useGetUsers();
 
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error! {error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error! {error.message}</p>;
 
-  return <UserList users={[]} />;
+  return <UserList users={users} />;
 };
